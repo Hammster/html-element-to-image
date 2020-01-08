289169116f4b8a98682f7adf0ef9d2707a5e13c1
Hans Koch <mail@hans-koch.me>
2020-01-08 12:03:53 +0100

    fixed type definiton

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,13 @@
-declare module "html-element-to-image" {
-    export interface ICaptureOptions {
-        targetNode: HTMLElement | Element
-        excludedNodes: HTMLElement[] | Element[]
-        padding: IDirections
-        margin: IDirections
-        returnType: string
-    }
-
-    export interface IDirections {
-        top: number
-        right: number
-        bottom: number
-        left: number
-    }
-
-    export default function NodeToDataURL (userConfig: Partial<ICaptureOptions>): Promise<string>
+export interface ICaptureOptions {
+    targetNode: HTMLElement | Element;
+    excludedNodes: HTMLElement[] | Element[];
+    customStyle: string;
+    returnType: string;
 }
+export interface IDirections {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+}
+export declare function NodeToDataURL(userConfig: Partial<ICaptureOptions>): Promise<string>;
