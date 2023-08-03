85d6ba339cd94f662c78bac93092767f53e51dfe
Hans Koch <mail@hans-koch.me>
2023-08-03 19:21:30 +0200

    upgrade rollup, remove yarn.lock, switch terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
-const rollupPluginTerser = require('rollup-plugin-terser')
+const terser = require('@rollup/plugin-terser');
 
 module.exports = {
     input: 'dist/index.js',
-    plugins: [rollupPluginTerser.terser()],
+    plugins: [terser()],
     output: [{
         file: 'dist/index.umd.min.js',
         name: 'nodeToDataURL',
         format: 'umd'
+    },{
+        file: 'dist/index.cjs.min.js',
+        format: 'cjs'   
     },{
         file: 'dist/index.min.js',
         format: 'es'
